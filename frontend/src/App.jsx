0c6742dd0977e634e535d4e8d54058e1936f148d
Abhinav Borah <imabhinavborah@gmail.com>
2024-03-08 19:23:36 +0530

    add fetch todos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import { CreateTodo } from "./components/CreateTodo";
 import { Todos } from "./components/Todos";
@@ -6,10 +6,12 @@ import { Todos } from "./components/Todos";
 function App() {
   const [todos, setTodos] = useState([]);
 
-  fetch("http://localhost:3000/todos").then(async function (res) {
-    const json = await res.json();
-    setTodos(json.todos);
-  });
+  useEffect(() => {
+    fetch("http://localhost:3000/todos").then(async (res) => {
+      const json = await res.json();
+      setTodos(json.todos);
+    });
+  }, [todos]);
 
   return (
     <div>
